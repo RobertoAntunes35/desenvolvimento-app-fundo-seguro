2ae6f117a40eff5852e2835a1bc6bd61342d95d8
RobertoAntunes <roh.antunes35@gmail.com>
2023-11-13 13:11:40 -0300

    adicionando funcionalidade para a aplicacao esperar o rabbitMQ iniciar

--- users-api/src/config/rabbitmq/rabbitConfig.js
+++ users-api/src/config/rabbitmq/rabbitConfig.js
@@ -5,11 +5,25 @@ import {RABBIT_MQ_URL} from '../secrets/secrets.js'
 import * as queues from './queue.js'
 
 const HALF_SECOND = 500
+const HALF_MINUTE = 30000;
+const CONTAINER_ENV = 'container';
 
 export async function connectRabbitMQ() {
+    const env = process.env.NODE_ENV;
+    console.info(env)
+    if(CONTAINER_ENV === env) {
+        console.info("Waiting for rabbitMQ to start ...");
+        setInterval(() => {
+            connectRabbitMqAndCreateQueues()
+        }, HALF_MINUTE)
+    } else {
+        await connectRabbitMqAndCreateQueues()
+    }
+}
+async function connectRabbitMqAndCreateQueues() {
     amqp.connect(RABBIT_MQ_URL, (error, connection) => {
         if (error) {
-            console.error(error.message);
+            throw error;
         }
         createQueue(connection, 
             queues.CREDITS_UPDATE_QUEUE,
@@ -31,20 +45,18 @@ export async function connectRabbitMQ() {
             queues.USER_CONFIRMATION_ROUTING_KEY,
             queues.DEBTS_TOPIC
             )
-        
-        
         setTimeout(function () {
             connection.close();
         }, HALF_SECOND)
-    });
-    function createQueue(connection, queue, routingKey, topic) {
-        connection.createChannel((error, channel) => {
-            if (error) {
-                throw error;
-            }
-            channel.assertExchange(topic, 'topic', {durable : true});
-            channel.assertQueue(queue, {durable : true});
-            channel.bindQueue(queue, topic, routingKey)
-        })
-    }
+    })
+}
+function createQueue(connection, queue, routingKey, topic) {
+    connection.createChannel((error, channel) => {
+        if (error) {
+            throw error;
+        }
+        channel.assertExchange(topic, 'topic', {durable : true});
+        channel.assertQueue(queue, {durable : true});
+        channel.bindQueue(queue, topic, routingKey)
+    })
 }
